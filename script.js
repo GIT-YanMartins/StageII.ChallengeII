799383759b5ac5e9d4ad9ae93d91bdb72389d0ad
GIT-YanMartins <yangabriel15@outlook.com>
2023-01-31 12:16:05 -0300

    sem alterações

---
+++ script.js
@@ -0,0 +1,46 @@
+
+const nome1 = prompt('Qual o seu nome?');
+const sobrenome = prompt ('Qual o seu sobrenome?')
+alert (`Olá ${nome1} ${sobrenome}! \n Eu preciso de ajuda com a ultima questão, por isso eu não fiz ela \n se possível, dê um git merge explicando ela, por favor `);
+
+
+//// EXERCISE I
+function msg(){
+console.log("yan")
+}
+msg()
+
+//// EXERCISE II
+
+const nome = function (nome){
+    console.log(`Olá, meu nome é ${nome}!`)
+}
+nome ("Yan Gabriel Martins, o sangue bom")
+
+//// EXERCISE III
+
+const info = function (nome, idade, gosto){
+ console.log(`Olá, meu nome é ${nome}, tenho ${idade} e gosto pra cacete de ${gosto}`)
+}
+      info(`Yan Gabriel Martins`, `23 anos`, `Rap e hip hop nacional`)
+
+//// EXERCISE IV
+
+function filmemusicado(filme, música){
+console.log (`Olá, eu gosto do filme ${filme} e do gênero musical ${música}`)
+}
+filmemusicado(`Interestelar`, `rap`)
+
+//// EXERCISE V
+
+function triploEnãoÉoX (a,b){
+    let y = (a*b)
+    return y
+}
+triploEnãoÉoX(3,3)
+console.log (triploEnãoÉoX(3,3))
+
+//// EXERCISE VI
+
+
+
